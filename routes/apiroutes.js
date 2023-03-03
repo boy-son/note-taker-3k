863d69a5af86096da13c7a307e3dd705fe372b83
boy-son <branden.wheat@yahoo.com>
2023-03-03 11:01:10 -0500

    worked out most of apiRoutes, added uuid but havent implemented

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,8 +3,32 @@ const path = require ('path');
 
 
 module.exports = (app) => {
+    const noteData = fs.readFileSync(path.join(__dirname, '../db/db.json'));
+    const noteDataArr = JSON.parse(noteData);
 
-app.get ('/api/notes',)
+app.get ('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'));
+});
+
+app.post('api/notes', (req,res) => {
+    const newNote = req.body;
+    noteDataArr.push(newNote);
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(noteDataArr));
+    res.json(noteDataArr);
+});
+
+app.get('api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    const note = noteDataArr.find(note => note.id === noteId);
+    res.json(note);
+});
+
+app.delete('api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    const newNoteDataArr = noteDataArr.filter(note => note.id !== noteId);
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(newNoteDataArr));
+    res.json(newNoteDataArr);
+});
     
 }
 
